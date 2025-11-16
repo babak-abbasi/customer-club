22fd78a38f324872e64730bb766d62b80b9e1f7c
babak abbasi <Babak.Abbasi.develop@gmail.com>
2025-11-16 18:35:26 +0330

    [fix] read projects added to sln

--- src/repository/Repository.Read/Repository/CountryReadRepository.cs
+++ src/repository/Repository.Read/Repository/CountryReadRepository.cs
@@ -42,4 +42,32 @@ public class CountryReadRepository: ICountryReadRepository
             throw new Exception($"ElasticSearch read failure: {exception.Message}");
         }
     }
+
+    public async Task<Country?> GetCountriesAsync()
+    {
+        if(client == null)
+            throw new Exception($"ElasticSearch connection failure while reading");
+
+        try
+        {
+            var response = await client.GetAsync<Country>(id, x => x.Index("country"));
+
+            if (!response.IsValidResponse)
+                throw new Exception($"Not Found.");
+
+            return new Country()
+            {
+                Id = id,
+                Code = response.Source.Code,
+                Name = response.Source.Name,
+                CreatedDate = response.Source.CreatedDate,
+                ModifiedDate = response.Source.ModifiedDate,
+                Order = response.Source.Order
+            };
+        }
+        catch(Exception exception)
+        {
+            throw new Exception($"ElasticSearch read failure: {exception.Message}");
+        }
+    }
 }
